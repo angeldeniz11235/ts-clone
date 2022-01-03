ff2bcc34ffed1063052a85eb8259a079740c362d
Angel D <angeldeniz11235@gmail.com>
2022-01-03 21:43:10 +0000

    trying to get strapi to work

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '932276a2bfb426051a8a9e1cac9c5bd7'),
-    },
-  },
-});
